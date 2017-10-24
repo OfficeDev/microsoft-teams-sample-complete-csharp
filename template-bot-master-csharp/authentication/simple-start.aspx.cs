36eda84556ff70fb9878fadefbcf483fd5d040b3
jotrick <jotrick@microsoft.com>
2017-10-25 08:39:02 +0900

    Added authentication example for static tab and simple graph call

---
+++ template-bot-master-csharp/authentication/simple-start.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace Microsoft.Teams.TemplateBotCSharp.src.tab
+{
+    public partial class tab : System.Web.UI.Page
+    {
+        private bool IsValidFilePath { get; set; }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            appId.Text = ConfigurationManager.AppSettings["MicrosoftAppId"].ToString(); 
+        }
+    }
+}
