9df2bdbe1d725e3639d81625cbe36dba171b8beb
jotrick <jotrick@microsoft.com>
2017-10-25 08:15:30 +0900

    Added instructions, Wikipedia compose extension

--- template-bot-master-csharp/controllers/MessagesController.cs
+++ template-bot-master-csharp/controllers/MessagesController.cs
@@ -8,6 +8,8 @@ using Microsoft.Bot.Connector;
 using System.Diagnostics;
 using Microsoft.Bot.Connector.Teams;
 using Microsoft.Bot.Connector.Teams.Models;
+using System.Collections.Generic;
+using Microsoft.Teams.TemplateBotCSharp.Properties;
 
 namespace Microsoft.Teams.Tutorial.CSharp
 {
@@ -47,6 +49,35 @@ namespace Microsoft.Teams.Tutorial.CSharp
                 if (activity.IsO365ConnectorCardActionQuery())
                 {
                     return await HandleO365ConnectorCardActionQuery(activity);
+                }
+                else if (activity.IsComposeExtensionQuery())
+                {
+                    if (activity.Name == "composeExtension/query")
+                    {
+                        var invokeResponse = this.GetComposeExtensionResponse(activity);
+                        return Request.CreateResponse<ComposeExtensionResponse>(HttpStatusCode.OK, invokeResponse);
+                    }
+                    else if (activity.Name == "composeExtension/querySettingUrl")
+                    {
+                        var invokeConfigResponse = WikiHelper.GetConfig();
+                        return Request.CreateResponse<ComposeExtensionResponse>(HttpStatusCode.OK, invokeConfigResponse);
+                    }
+                    else if (activity.Name == "composeExtension/setting")
+                    {
+                        var composeExtensionQuery = activity.GetComposeExtensionQueryData();
+                        if (!string.IsNullOrEmpty(composeExtensionQuery.State))
+                        {
+                            StateClient stateClient = activity.GetStateClient();
+                            BotData userData = stateClient.BotState.GetUserData(activity.ChannelId, activity.From.Id);
+                            WikiHelper.ParseSettingsAndSave(composeExtensionQuery.State, userData, stateClient, activity);
+                            return Request.CreateResponse<ComposeExtensionResponse>(HttpStatusCode.OK, null);
+                        }
+                    }
+                    else
+                    {
+                        // Unknown compose extension type
+                        return Request.CreateResponse<ComposeExtensionResponse>(HttpStatusCode.OK, null);
+                    }
                 }
             }
             else
@@ -94,6 +125,122 @@ namespace Microsoft.Teams.Tutorial.CSharp
 
             await connectorClient.Conversations.ReplyToActivityWithRetriesAsync(replyActivity);
             return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+
+        private ComposeExtensionResponse GetComposeExtensionResponse(Activity activity)
+        {
+            StateClient stateClient = activity.GetStateClient();
+            BotData userData = stateClient.BotState.GetUserData(activity.ChannelId, activity.From.Id);
+            var composeExtensionQuery = activity.GetComposeExtensionQueryData();
+
+            if (composeExtensionQuery.CommandId == null || composeExtensionQuery.Parameters == null)
+            {
+                return null;
+            }
+
+            var initialRunParameter = WikiHelper.GetQueryParameterByName(composeExtensionQuery, Strings.manifestInitialRun);
+            var queryParameter = WikiHelper.GetQueryParameterByName(composeExtensionQuery, Strings.manifestParameterName);
+
+            if (userData == null)
+            {
+                var composeExtensionResponse = new ComposeExtensionResponse();
+                string message = Strings.ComposeExtensionNoUserData;
+                composeExtensionResponse.ComposeExtension = WikiHelper.GetMessageResponseResult(message);
+                return composeExtensionResponse;
+            }
+
+            /**
+            * Below are the checks for various states that may occur
+            * Note that the order of many of these blocks of code is important
+            */
+
+            // Situation where the incoming payload was received from the config popup
+
+            if (!string.IsNullOrEmpty(composeExtensionQuery.State))
+            {
+                WikiHelper.ParseSettingsAndSave(composeExtensionQuery.State, userData, stateClient, activity);
+
+                // Need to keep going to return a response so do not return here
+
+                queryParameter = "";
+                initialRunParameter = "true";
+            }
+
+            // This is a sitaution where the user's preferences have not been set up yet
+            if (string.IsNullOrEmpty(userData.GetProperty<string>(Strings.ComposeExtensionCardTypeKeyword)))
+            {
+                var composeExtensionResponse = WikiHelper.GetConfig();
+                return composeExtensionResponse;
+            }
+
+            // This is the situation where the user has entered the word 'reset' and wants
+            // to clear his/her settings
+            // resetKeyword for English is "reset"
+            // **************** NOTE!!!!!!! - it is not recommended that you have a reset
+            // keyword like this in your real app - this is simply meant for testing purposes
+
+            if (string.Equals(queryParameter.ToLower(), Strings.ComposeExtensionResetKeyword))
+            {
+                // Reset the user's state
+                userData.SetProperty<string>("composeExtensionCardType", null);
+                stateClient.BotState.SetUserData(activity.ChannelId, activity.From.Id, userData);
+                var composeExtensionResponse = new ComposeExtensionResponse();
+                composeExtensionResponse.ComposeExtension = WikiHelper.GetMessageResponseResult(Strings.ComposeExtensionResetText);
+                return composeExtensionResponse;
+            }
+
+
+            // This is the situation where the user in on the initial run of the compose extension
+            // e.g. when the user first goes to the compose extension and the search bar is still blank
+            // in order to get the compose extension to run the initial run, the setting "initialRun": true
+            // must be set in the manifest for the compose extension
+
+            if (initialRunParameter == "true")
+            {
+                // Signin Experience, uncomment below code for Signin Experience
+                // var composeExtensionResponse = WikiHelper.GetSignin(composeExtensionResponse);
+                // return composeExtensionResponse;
+
+                var composeExtensionResponse = new ComposeExtensionResponse();
+                composeExtensionResponse.ComposeExtension = WikiHelper.GetMessageResponseResult(Strings.ComposeExtensionInitialRunText);
+                return composeExtensionResponse;
+            }
+
+            /**
+            * Below here is simply the logic to call the Wikipedia API and create the response for
+            * a query; the general flow is to call the Wikipedia API for the query and then call the
+            * Wikipedia API for each entry for the query to see if that entry has an image; in order
+            * to get the asynchronous sections handled, an array of Promises for cards is used; each
+            * Promise is resolved when it is discovered if an image exists for that entry; once all
+            * of the Promises are resolved, the response is sent back to Teams
+            */
+
+            WikiResult wikiResult = WikiHelper.SearchWiki(queryParameter, composeExtensionQuery);
+            List<ComposeExtensionAttachment> composeExtensionAttachments = new List<ComposeExtensionAttachment>();
+
+            // enumerate search results and build Promises for cards for response
+            foreach (var searchResult in wikiResult.query.search)
+            {
+                //Get the Image result on the basis of Image Title one by one
+                ImageResult imageResult = WikiHelper.SearchWikiImage(searchResult);
+
+                //Get the Image Url from imageResult
+                string imageUrl = WikiHelper.GetImageURL(imageResult);
+
+                //Set the Highlighter title
+                string highlightedTitle = WikiHelper.GetHighLightedTitle(searchResult.title, queryParameter);
+
+                string cardText = searchResult.snippet + " ...";
+
+                // create the card itself and the preview card based upon the information
+                // check user preference for which type of card to create
+
+                composeExtensionAttachments.Add(TemplateUtility.CreateComposeExtensionCardsAttachments(highlightedTitle, cardText, imageUrl, userData.GetProperty<string>(Strings.ComposeExtensionCardTypeKeyword)));
+            }
+
+            var wikiComposeExtensionResponse = WikiHelper.GetComposeExtenionQueryResult(composeExtensionAttachments);
+
+            return wikiComposeExtensionResponse;
         }
 
         private Activity HandleSystemMessage(Activity message)
---
+++ template-bot-master-csharp/utilities/WikiHelper.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Teams.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Microsoft.Teams.Tutorial.CSharp
+{
+    public static class WikiHelper
+    {
+        const string searchApiUrlFormat = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=[keyword]&srlimit=[limit]&sroffset=[offset]&format=json";
+        const string imageApiUrlFormat = "https://en.wikipedia.org/w/api.php?action=query&formatversion=2&format=json&prop=pageimages&piprop=thumbnail&pithumbsize=250&titles=[title]";
+
+        // return the value of the specified query parameter
+        public static string GetQueryParameterByName(ComposeExtensionQuery query, string name)
+        {
+            if (query.Parameters[0].Name == name)
+            {
+                return query.Parameters[0].Value.ToString();
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        // used to parse the user preferences from the state and save them for later use
+        public static void ParseSettingsAndSave(string state, BotData userData, StateClient stateClient, Activity activity)
+        {
+            userData.SetProperty<string>("composeExtensionCardType", state);
+            stateClient.BotState.SetUserData(activity.ChannelId, activity.From.Id, userData);
+        }
+
+        public static ComposeExtensionResponse GetConfig()
+        {
+            string configUrl = ConfigurationManager.AppSettings["BaseUri"].ToString() + "/composeExtensionSettings.html";
+            CardAction configExp = new CardAction(ActionTypes.OpenUrl, "Config", null, configUrl);
+            List<CardAction> lstCardAction = new List<CardAction>();
+            lstCardAction.Add(configExp);
+            var composeExtensionResponse = new ComposeExtensionResponse();
+            ComposeExtensionResult composeExtensionResult = new ComposeExtensionResult();
+
+            ComposeExtensionSuggestedAction objSuggestedAction = new ComposeExtensionSuggestedAction();
+            objSuggestedAction.Actions = lstCardAction;
+
+            composeExtensionResult.SuggestedActions = objSuggestedAction;
+            composeExtensionResult.Type = "config";
+            composeExtensionResponse.ComposeExtension = composeExtensionResult;
+
+            return composeExtensionResponse;
+        }
+
+        public static ComposeExtensionResponse GetSignin(ComposeExtensionResponse composeExtensionResponse)
+        {
+            string configUrl = ConfigurationManager.AppSettings["BaseUri"].ToString() + "/composeExtensionSettings.html";
+            CardAction configExp = new CardAction(ActionTypes.OpenUrl, "Config", null, configUrl);
+            List<CardAction> lstCardAction = new List<CardAction>();
+            lstCardAction.Add(configExp);
+            composeExtensionResponse = new ComposeExtensionResponse();
+            ComposeExtensionResult composeExtensionResult = new ComposeExtensionResult();
+
+            ComposeExtensionSuggestedAction objSuggestedAction = new ComposeExtensionSuggestedAction();
+            objSuggestedAction.Actions = lstCardAction;
+
+            composeExtensionResult.SuggestedActions = objSuggestedAction;
+            composeExtensionResult.Type = "auth";
+            composeExtensionResponse.ComposeExtension = composeExtensionResult;
+
+            return composeExtensionResponse;
+        }
+
+        public static ComposeExtensionResult GetMessageResponseResult(string message)
+        {
+            ComposeExtensionResult composeExtensionResult = new ComposeExtensionResult();
+            composeExtensionResult.Type = "message";
+            composeExtensionResult.Text = message;
+            return composeExtensionResult;
+        }
+
+        public static WikiResult SearchWiki(string queryParameter, ComposeExtensionQuery composeExtensionQuery)
+        {
+            string searchApiUrl = searchApiUrlFormat.Replace("[keyword]", queryParameter);
+            searchApiUrl = searchApiUrl.Replace("[limit]", composeExtensionQuery.QueryOptions.Count + "");
+            searchApiUrl = searchApiUrl.Replace("[offset]", composeExtensionQuery.QueryOptions.Skip + "");
+            WikiResult wikiResult = null;
+
+            // call Wikipedia API to search
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(searchApiUrl);
+                using (HttpWebResponse response = (HttpWebResponse)myRequest.GetResponse())
+                {
+                    string ResponseText;
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        ResponseText = reader.ReadToEnd();
+                        wikiResult = JsonConvert.DeserializeObject<WikiResult>(ResponseText);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return wikiResult;
+        }
+
+        public static ImageResult SearchWikiImage(Search wikiSearch)
+        {
+            ImageResult imageResult = null;
+            // a separate API call to Wikipedia is needed to fetch the page image, if it exists
+            string imageApiUrl = imageApiUrlFormat.Replace("[title]", wikiSearch.title);
+
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(imageApiUrl);
+                using (HttpWebResponse response = (HttpWebResponse)myRequest.GetResponse())
+                {
+                    string ResponseText;
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        ResponseText = reader.ReadToEnd();
+                        imageResult = JsonConvert.DeserializeObject<ImageResult>(ResponseText);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return imageResult;
+        }
+
+        public static string GetImageURL(ImageResult imageResult)
+        {
+            string imageUrl = string.Empty;
+
+            if (imageResult != null && imageResult.query.pages != null && imageResult.query.pages.Count > 0 && imageResult.query.pages[0].thumbnail != null)
+            {
+                imageUrl = imageResult.query.pages[0].thumbnail.source;
+            }
+            else
+            {
+                // no image so use default Wikipedia image
+                imageUrl = "https://upload.wikimedia.org/wikipedia/commons/d/de/Wikipedia_Logo_1.0.png";
+            }
+
+            return imageUrl;
+        }
+
+        public static string GetHighLightedTitle(string title, string queryParameter)
+        {
+            // highlight matched keyword
+            string highlightedTitle = title;
+
+            if (queryParameter != null)
+            {
+                var regex = new Regex(queryParameter, RegexOptions.IgnoreCase);
+                highlightedTitle = regex.Replace(highlightedTitle, "<b>" + queryParameter + "</b>");
+            }
+
+            // make title into a link
+            string linkTitle = "<a href=\"" + "https://en.wikipedia.org/wiki/" + title + "\" target=\"_blank\">" + highlightedTitle + "</a>";
+
+            return linkTitle;
+        }
+
+        public static ComposeExtensionResponse GetComposeExtenionQueryResult(List<ComposeExtensionAttachment> lstComposeExtensionAttachment)
+        {
+            var composeExtensionResponse = new ComposeExtensionResponse();
+            ComposeExtensionResult composeExtensionResult = new ComposeExtensionResult();
+            composeExtensionResult.Type = "result";
+            composeExtensionResult.Attachments = lstComposeExtensionAttachment;
+            composeExtensionResult.AttachmentLayout = "list";
+            composeExtensionResponse.ComposeExtension = composeExtensionResult;
+
+            return composeExtensionResponse;
+        }
+    }
+
+    //Wiki Json Result Object Classes
+    public class Continue
+    {
+        public int sroffset { get; set; }
+        public string @continue { get; set; }
+    }
+
+    public class Searchinfo
+    {
+        public int totalhits { get; set; }
+    }
+
+    public class Search
+    {
+        public int ns { get; set; }
+        public string title { get; set; }
+        public int pageid { get; set; }
+        public int size { get; set; }
+        public int wordcount { get; set; }
+        public string snippet { get; set; }
+        public string timestamp { get; set; }
+    }
+
+    public class Query
+    {
+        public Searchinfo searchinfo { get; set; }
+        public List<Search> search { get; set; }
+    }
+
+    public class WikiResult
+    {
+        public string batchcomplete { get; set; }
+        public Continue @continue { get; set; }
+        public Query query { get; set; }
+    }
+
+    /// <summary>
+    /// Image Json Object Classes
+    /// </summary>
+    public class Normalized
+    {
+        public bool fromencoded { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        public string source { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+
+    public class Page
+    {
+        public int pageid { get; set; }
+        public int ns { get; set; }
+        public string title { get; set; }
+        public Thumbnail thumbnail { get; set; }
+    }
+
+    public class QueryImage
+    {
+        public List<Normalized> normalized { get; set; }
+        public List<Page> pages { get; set; }
+    }
+
+    public class ImageResult
+    {
+        public bool batchcomplete { get; set; }
+        public QueryImage query { get; set; }
+    }
+}
